aa6b9c2fea962fc5f2ccad50f35389bf44b64f1a
Rakip Bebo <reobebo1@hotmail.com>
2020-10-18 01:17:19 -0500

    bootstrap sass admin theme

---
+++ src/js/custom.js
@@ -0,0 +1,7 @@
+(function(){
+    "use strict";
+    $('[data-toggle="sidebar').click(function(event){
+       event.preventDefault();
+       $('.app').toggleClass('sidebar-toggled');
+    });})
+();
